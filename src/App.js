0154ab29163996330bea331e6597887b7fa2dd56
Hypermicro-code <tony2mat@gmail.com>
2025-04-25 18:39:20 +0200

    Add files via upload
    Testversjon av MC-Tur planlegger

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to RideAway!</h1>
+      <p>Your motorcycle travel planner prototype is running.</p>
+    </div>
+  );
+}
+
+export default App;
