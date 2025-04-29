20be90dc8c6d6f49a44de84a5bbe66e19bd2f1e3
Hypermicro-code <tony2mat@gmail.com>
2025-04-29 07:36:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import TurDetaljer from './TurDetaljer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/tur/:id" element={<TurDetaljer />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
