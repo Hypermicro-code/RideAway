6ecf8bdbd9aa617c691da1368c92ef7941b3badb
Hypermicro-code <tony2mat@gmail.com>
2025-04-29 14:36:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import TurDetaljer from './TurDetaljer';
+import PlanleggTur from './PlanleggTur';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +12,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/tur/:id" element={<TurDetaljer />} />
+        <Route path="/planlegg" element={<PlanleggTur />} />
       </Routes>
     </Router>
   </React.StrictMode>
