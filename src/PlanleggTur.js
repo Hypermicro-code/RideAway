e618917fd9b50421dea81ad7b57cfa4e857ff9a6
Hypermicro-code <tony2mat@gmail.com>
2025-05-01 19:51:31 +0200

    Update PlanleggTur.js

--- src/PlanleggTur.js
+++ src/PlanleggTur.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import Kartvisning from './Kartvisning';
+import Kartvisning from './Kartvisning'; //
 
 function PlanleggTur() {
   const { id } = useParams();
